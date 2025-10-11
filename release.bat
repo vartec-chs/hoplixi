9a6e4037a83292bfe90efd4be9856aefeb027b02
Vartec <misticmvm@gmail.com>
2025-10-12 01:11:28 +0300

    feat(release): обновить скрипт сборки для поддержки Windows MSIX и заменить команды flutter на fastforge feat(msix): добавить конфигурационный файл для сборки MSIX

--- release.bat
+++ release.bat
@@ -3,19 +3,24 @@
 echo Choose platform for build:
 echo 1. Windows
 echo 2. Android
+echo 3. Windows MSIX
 
 set /p choice="Enter number: "
 
 if "%choice%"=="1" (
     echo Building for Windows...
-    flutter build windows
+    fastforge package --platform windows --targets exe
 ) else (
     if "%choice%"=="2" (
         echo Building for Android...
-        flutter build apk --release
+        fastforge package --platform android --targets apk
     ) else (
-        echo Invalid choice. Please try again.
-        goto :eof
+        if "%choice%"=="3" (
+            echo Building for Windows MSIX...
+            fastforge package --platform windows --targets msix
+        ) else (
+            echo Invalid choice. Please try again.
+            goto :eof
     )
 )
 
