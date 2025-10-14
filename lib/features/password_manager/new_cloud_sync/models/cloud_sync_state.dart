23f6734f098b166f0927f1abcba0562e2116885f
Vartec <misticmvm@gmail.com>
2025-10-14 23:56:40 +0300

    refactor(models): повысить компактность кода в CloudSyncState
    Удалены избыточные пустые строки между фабричными конструкторами в классе `CloudSyncState`.
    
    Это изменение направлено на улучшение читаемости и приведение кода к более единообразному и сжатому стилю форматирования.

--- lib/features/password_manager/new_cloud_sync/models/cloud_sync_state.dart
+++ lib/features/password_manager/new_cloud_sync/models/cloud_sync_state.dart
@@ -16,20 +16,16 @@ abstract class CloudSyncProgress with _$CloudSyncProgress {
 @freezed
 abstract class CloudSyncState with _$CloudSyncState {
   const factory CloudSyncState.idle() = _Idle;
-  
+
   const factory CloudSyncState.exporting({
     required CloudSyncProgress progress,
   }) = _Exporting;
-  
+
   const factory CloudSyncState.importing({
     required CloudSyncProgress progress,
   }) = _Importing;
-  
-  const factory CloudSyncState.success({
-    required String message,
-  }) = _Success;
-  
-  const factory CloudSyncState.error({
-    required String message,
-  }) = _Error;
+
+  const factory CloudSyncState.success({required String message}) = _Success;
+
+  const factory CloudSyncState.error({required String message}) = _Error;
 }
